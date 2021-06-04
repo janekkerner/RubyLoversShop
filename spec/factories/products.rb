713f1db9d6db39c342b2ff200b09895decc4f432
janekkerner <janek.kerner@gmail.com>
2021-06-04 16:40:18 +0200

    tests update

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :product do
     sequence(:name) { |n| "Product no. #{n}" }
-    price { "9.99" }
+    price { '9.99' }
   end
 end
--- spec/system/products_spec.rb
+++ spec/system/products_spec.rb
@@ -1,22 +1,33 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe "Products", type: :system do
-  
+RSpec.describe 'Products', type: :system do
   let!(:product) { create(:product) }
-  let!(:product2) { create(:product) }
 
   before do
     driven_by(:rack_test)
   end
 
-  describe "when user visit home page" do
-
-    it 'should see all products' do
+  describe 'when user visit home page' do
+    it 'product has a name value' do
       visit root_path
       within '.row#products' do
         expect(page).to have_text(product.name)
-        expect(page).to have_text(product2.name)
-        expect(page).to have_css('.card-img-top', count: 2 )
+      end
+    end
+
+    it 'product has a price value' do
+      visit root_path
+      within '.row#products' do
+        expect(page).to have_text(product.price)
+      end
+    end
+
+    it 'see all products' do
+      visit root_path
+      within '.row#products' do
+        expect(page).to have_css('.card-img-top', count: 1)
       end
     end
   end
