d1357750614d0b97b67e26d1476ffbdc0fcfd902
janekkerner <janek.kerner@gmail.com>
2021-06-06 20:07:35 +0200

    Brand model created

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,5 +3,6 @@
 class Product < ApplicationRecord
   has_one_attached :image
   belongs_to :category
+  belongs_to :brand, optional: true
   scope :with_category, ->(category) { Product.where(category_id: category) }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_04_181445) do
-
+ActiveRecord::Schema.define(version: 2021_06_06_175536) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +43,12 @@ ActiveRecord::Schema.define(version: 2021_06_04_181445) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "brands", force: :cascade do |t|
+    t.string "brand_name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
@@ -56,10 +61,13 @@ ActiveRecord::Schema.define(version: 2021_06_04_181445) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "category_id", null: false
+    t.bigint "brand_id"
+    t.index ["brand_id"], name: "index_products_on_brand_id"
     t.index ["category_id"], name: "index_products_on_category_id"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "products", "brands"
   add_foreign_key "products", "categories"
 end
