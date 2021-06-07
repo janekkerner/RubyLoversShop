27463bb0c7c1be2ce488175c3ccbe2ad28937803
janekkerner <janek.kerner@gmail.com>
2021-06-07 17:59:11 +0200

    feature with brand filter ready

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,17 +2,24 @@
 
 class PagesController < ApplicationController
   def home
+    @filters = {}
+    @filters[:category_id] =  params[:category_id] if params[:category_id].present?
+    @filters[:brand_id] = params[:brand_id] if params[:brand_id].present?
     @categories = Category.all
-    render :home, layout: true, locals: { products: product_collection }
+    @brands = Brand.all
+    render :home, locals: { products: product_collection }
   end
 
   private
 
   def product_collection
-    if params[:category_id].nil?
+    if @filters[:category_id].nil? && @filters[:brand_id].nil?
       Product.order('created_at DESC')
     else
-      Product.with_category(params[:category_id])
+      products = Product.all
+      products = products.with_category(@filters[:category_id]) if @filters[:category_id].present?
+      products = products.with_brand(@filters[:brand_id]) if @filters[:brand_id].present?
+      return products.order('created_at DESC')
     end
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -4,5 +4,6 @@ class Product < ApplicationRecord
   has_one_attached :image
   belongs_to :category
   belongs_to :brand, optional: true
-  scope :with_category, ->(category) { Product.where(category_id: category) }
+  scope :with_category, ->(category) { where category_id: category }
+  scope :with_brand, ->(brand) { where brand_id: brand }
 end
