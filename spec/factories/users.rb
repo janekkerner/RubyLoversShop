f4559af96f33f52de724a1825e3b7ddb604ea307
janekkerner <janek.kerner@gmail.com>
2021-06-11 23:19:30 +0200

    system tests added for sign up feature

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,8 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :user do
-    
+    sequence(:email) { |n| "exampleemail#{n}@example.com" }
+    password { 'P@ssword123' }
   end
 end
--- spec/system/authentications_spec.rb
+++ spec/system/authentications_spec.rb
@@ -1,12 +1,32 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe "Authentications", type: :system do
+RSpec.describe 'Authentications', type: :system do
+  let(:user) { build(:user) }
+  let(:user2) { create(:user) }
+
   before do
     driven_by(:rack_test)
   end
 
-  describe "when user is visiting sign up page" do
-    it "can create a new account" do
+  describe 'when user is visiting sign up page' do
+    it 'can create a new account' do
+      visit new_user_registration_path
+      fill_in 'user_email', with: user.email
+      fill_in 'user_password', with: user.password
+      fill_in 'user_password_confirmation', with: user.password
+      click_button 'Sign up'
+      expect(page).to have_text('Welcome! You have signed up successfully.')
+    end
+
+    it 'can not create new account with already taken email' do
+      visit new_user_registration_path
+      fill_in 'user_email', with: user2.email
+      fill_in 'user_password', with: user2.password
+      fill_in 'user_password_confirmation', with: user2.password
+      click_button 'Sign up'
+      expect(page).to have_text('Email has already been taken')
     end
   end
 end
