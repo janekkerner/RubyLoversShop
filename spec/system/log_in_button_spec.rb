0686ee3db9dbadadd5f7eb887e79f266ad5aa301
janekkerner <janek.kerner@gmail.com>
2021-06-16 10:19:06 +0200

    rubocop check for log in button feature

--- spec/system/log_in_button_spec.rb
+++ spec/system/log_in_button_spec.rb
@@ -1,20 +1,22 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe "Authentications", type: :system do
+RSpec.describe 'Authentications', type: :system do
   before do
     driven_by(:rack_test)
   end
 
-  describe "when user is visiting different pages" do
-    it "can log in with link from root path" do
+  describe 'when user is visiting different pages' do
+    it 'can log in with link from root path' do
       visit root_path
-      click_link "Log in"
+      click_link 'Log in'
       expect(page).to have_selector('h2', text: 'Log in')
     end
 
     it 'can log in with link from sign up page' do
       visit new_user_registration_path
-      click_link "Log in", match: :first
+      click_link 'Log in', match: :first
       expect(page).to have_selector('h2', text: 'Log in')
     end
   end
