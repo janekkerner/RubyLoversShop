d1e033c69606391cfd91859c8d64a0ffa6fe2b27
janekkerner <janek.kerner@gmail.com>
2021-06-16 10:17:12 +0200

    log in button added to nav

---
+++ spec/system/log_in_button_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe "Authentications", type: :system do
+  before do
+    driven_by(:rack_test)
+  end
+
+  describe "when user is visiting different pages" do
+    it "can log in with link from root path" do
+      visit root_path
+      click_link "Log in"
+      expect(page).to have_selector('h2', text: 'Log in')
+    end
+
+    it 'can log in with link from sign up page' do
+      visit new_user_registration_path
+      click_link "Log in", match: :first
+      expect(page).to have_selector('h2', text: 'Log in')
+    end
+  end
+end
