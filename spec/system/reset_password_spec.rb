e872a1e9033ba48965a32e5c7a97d4c7fd51c926
janekkerner <33815203+janekkerner@users.noreply.github.com>
2021-06-17 09:45:45 +0200

    log in feature (#6)
    * view edited and test added for log in feature
    
    * sidebar view updated
    
    * reset password feature (#7)
    
    * reset password feature with system tests
    
    * rubocop check
    
    * Sign in button feature (#8)
    
    * log in button added to nav
    
    * rubocop check for log in button feature
    
    * sign out button added with system test (#9)

---
+++ spec/system/reset_password_spec.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Authentications', type: :system do
+  let(:user) { create(:user) }
+  let(:user2) { build(:user) }
+
+  before do
+    driven_by(:rack_test)
+  end
+
+  describe 'when user visit log in page' do
+    it 'can reset his password' do
+      visit new_user_session_path
+      click_link 'Forgot your password?'
+      fill_in 'user_email', with: user.email
+      click_button 'Send me reset password instructions'
+      expect(page).to have_text('You will receive an email with instructions on how to reset your password in a few minutes.')
+      p
+    end
+
+    it 'cannot reset password for not existing user' do
+      visit new_user_session_path
+      click_link 'Forgot your password?'
+      fill_in 'user_email', with: user2.email
+      click_button 'Send me reset password instructions'
+      expect(page).to have_text('Email not found')
+    end
+  end
+end
