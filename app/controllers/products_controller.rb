c3410658107661b30b8891f5e589cb1b86b84737
janekkerner <janek.kerner@gmail.com>
2021-07-04 23:07:16 +0200

    removed unused private method from products controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,10 +6,4 @@ class ProductsController < ApplicationController
     @products = @q.result.includes(:category, :brand).order('created_at DESC')
     render :index, locals: { products: @products }
   end
-
-  private
-
-  def product_params
-    params.require(:product).permit(:name, :price, :image, :category_id, :brand_id)
-  end
 end
