adde08ec32258b6abf19932a04821227176f10ef
janekkerner <janek.kerner@gmail.com>
2021-07-05 17:20:34 +0200

    product update method added to controller

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -3,6 +3,7 @@
 module Admin
   class ProductsController < ApplicationController
     before_action :authenticate_admin_user!
+    before_action :set_product, only: [:edit, :update, :destroy]
 
     layout 'dashboard'
 
@@ -21,8 +22,20 @@ module Admin
       end
     end
 
+    def edit
+    end
+
+    def update
+      if product.update_all(product_params)
+        flash[:success] = "Product with ID: #{params[:id]} has been updated"
+        redirect_to admin_path
+      else
+        flash[:error] = "There was an error. Your product was not updated"
+        render :edit
+      end
+    end
+
     def destroy
-      @product = Product.find(params[:id])
       if @product.destroy
         flash[:success] = "Product with ID: #{params[:id]} has been deleted"
         redirect_to admin_path
@@ -30,6 +43,10 @@ module Admin
     end
 
     private
+
+    def set_product
+      @product = Product.find(params[:id])
+    end
 
     def product_params
       params.require(:product).permit(:name, :price, :image, :category_id, :brand_id)
