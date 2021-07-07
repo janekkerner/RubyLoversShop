116a8fa78e032628ae38eb3e8fc81824776a7dce
janekkerner <33815203+janekkerner@users.noreply.github.com>
2021-07-07 23:16:37 +0200

    Admin product destroy feature (#14)
    * destroy product feature added
    
    * system and request tests added
    
    * destroy method updated
    
    * Admin product update feature (#15)
    
    * product update method added to controller
    
    * system spec added
    
    * request tests added
    
    * tests styling issues resolved and rubocop config edited

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -3,6 +3,7 @@
 module Admin
   class ProductsController < ApplicationController
     before_action :authenticate_admin_user!
+    before_action :set_product, only: %i[edit update destroy]
 
     layout 'dashboard'
 
@@ -21,7 +22,34 @@ module Admin
       end
     end
 
+    def edit
+      render :edit, locals: { product: @product }
+    end
+
+    def update
+      if @product.update(product_params)
+        flash[:success] = "Product with ID: #{params[:id]} has been updated"
+        redirect_to edit_admin_product_path(@product.id)
+      else
+        flash[:error] = @product.errors.full_messages.to_sentence
+        render :edit, locals: { product: @product }
+      end
+    end
+
+    def destroy
+      if @product.destroy
+        flash[:success] = "Product with ID: #{params[:id]} has been deleted"
+      else
+        flash[:error] = 'There was an error. Desired product has not been deleted'
+      end
+      redirect_to admin_path
+    end
+
     private
+
+    def set_product
+      @product = Product.find(params[:id])
+    end
 
     def product_params
       params.require(:product).permit(:name, :price, :image, :category_id, :brand_id)
