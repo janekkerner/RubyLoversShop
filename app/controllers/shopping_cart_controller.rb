d8c2cc283a3e95cca9d4620cd2147f050aebfa51
janekkerner <janek.kerner@gmail.com>
2021-07-15 00:06:21 +0200

    product show method and view added

--- app/controllers/shopping_cart_controller.rb
+++ app/controllers/shopping_cart_controller.rb
@@ -7,4 +7,13 @@ class ShoppingCartController < ApplicationController
     shopping_cart_products = current_user.shopping_cart.products
     render :show, locals: { products: shopping_cart_products }
   end
+
+  def add_product_to_cart
+    if current_user.shopping_cart.cart_items.where(product_id: params[:id]).exists?
+      flash[:error] = 'Product is already in your cart'
+    else
+      current_user.shopping_cart.cart_items.create(product_id: params[:id])
+    end
+    redirect_to cart_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :admin_users, path: '/admin'
   devise_for :users, controllers: { registrations: 'users/registrations' }
   get '/products', to: 'products#index'
+  resources :products
 
   namespace :admin do
     get '/', to: 'dashboards#index'
@@ -13,4 +14,5 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   get 'cart', to: 'shopping_cart#show', as: 'cart'
+  post 'cart/:id', to: 'shopping_cart#add_product_to_cart', as: 'add_product_to_cart'
 end
