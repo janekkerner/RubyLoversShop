604ac82920f8e2df971fd0d9b6a318b36a12dd44
janekkerner <janek.kerner@gmail.com>
2021-07-20 09:36:53 +0200

    shopping cart controller updated

--- app/controllers/shopping_cart_controller.rb
+++ app/controllers/shopping_cart_controller.rb
@@ -2,9 +2,16 @@
 
 class ShoppingCartController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_cart
 
   def show
-    shopping_cart_products = current_user.shopping_cart.products
+    shopping_cart_products = @cart.products
     render :show, locals: { products: shopping_cart_products }
+  end
+
+  private
+
+  def set_cart
+    @cart = current_user.shopping_cart || current_user.create_shopping_cart
   end
 end
--- lib/production_tasks.rb
+++
@@ -1,3 +0,0 @@
-User.all.find_each do |user|
-  user.create_shopping_cart if user.shopping_cart == nil
-end
