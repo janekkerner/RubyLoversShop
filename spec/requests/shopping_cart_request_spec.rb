b5e047583c7b125b9e9314813d981ffc41425acd
Jan <janek.kerner@gmail.com>
2021-07-24 11:15:20 +0200

    add to cart button added for homepage products

--- spec/requests/shopping_cart_request_spec.rb
+++ spec/requests/shopping_cart_request_spec.rb
@@ -4,6 +4,7 @@ require 'rails_helper'
 
 RSpec.describe 'ShoppingCarts', type: :request do
   let!(:user) { create(:user) }
+  let!(:product) { create(:product) }
 
   describe 'GET /cart' do
     it 'has redirect http status when user not sign in' do
@@ -21,6 +22,20 @@ RSpec.describe 'ShoppingCarts', type: :request do
       sign_in user
       get '/cart'
       expect(response.body).to include('Your shopping cart is empty')
+    end
+  end
+
+  describe "POST /cart" do
+    it 'has success http status when user is signed in' do
+      sign_in user
+      post "/cart/#{product.id}"
+      follow_redirect!
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'has redirect status when user not sign in' do
+      post "/cart/#{product.id}"
+      expect(response).to have_http_status(:redirect)
     end
   end
 end
