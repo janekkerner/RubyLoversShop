9ccc9022ec87d3a7c8695d0a64a5a9f34a9d0554
Jan <janek.kerner@gmail.com>
2021-08-30 21:23:19 +0200

    description added to view

--- spec/system/products_spec.rb
+++ spec/system/products_spec.rb
@@ -10,25 +10,40 @@ RSpec.describe 'Products', type: :system do
   end
 
   describe 'when user visit home page' do
-    it 'product has a name value' do
+    before do
       visit root_path
+    end
+
+    it 'product has a name value' do
       within '.row#products' do
         expect(page).to have_text(product.name)
       end
     end
 
     it 'product has a price value' do
-      visit root_path
       within '.row#products' do
         expect(page).to have_text(product.price)
       end
     end
 
     it 'see all products' do
-      visit root_path
       within '.row#products' do
         expect(page).to have_css('.card-img-top', count: 1)
       end
+    end
+
+    it 'can go to product page by clicking the product name' do
+      within '.row#products' do
+        click_link product.name
+      end
+      expect(page).to have_selector("#product-#{product.id}-page")
+    end
+
+    it 'can go to product page by clicking the product photo' do
+      within '.row#products' do
+        find_by_id("product-#{product.id}-photo").click
+      end
+      expect(page).to have_selector("#product-#{product.id}-page")
     end
   end
 end
