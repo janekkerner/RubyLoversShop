ff8ad4d11a522804937375132f77856353492d4f
Jan <janek.kerner@gmail.com>
2021-11-20 11:48:15 +0100

    test: explicit subject added

--- spec/requests/cart_item_spec.rb
+++ spec/requests/cart_item_spec.rb
@@ -20,19 +20,21 @@ RSpec.describe 'CartItem', type: :request do
     context 'when user logged in' do
       before do
         sign_in user
-        request
       end
 
       context 'with no quantity params provided' do
         it 'has success http status' do
+          request
           expect(response).to have_http_status(:success)
         end
 
         it 'renders success notification after adding product to cart' do
+          request
           expect(response.body).to include("Product #{product.name} has been added to your shopping cart")
         end
 
         it 'increase quantity of cart item if same product is added to shopping cart' do
+          request
           post "/cart/#{product.id}"
           expect(user.shopping_cart.cart_items.find_by(product_id: product.id).quantity).to eq(2)
         end
@@ -42,10 +44,12 @@ RSpec.describe 'CartItem', type: :request do
         let(:params) { { quantity: 5 } }
 
         it 'can add product to shopping cart with specified quantity' do
+          request
           expect(user.shopping_cart.cart_items.find_by(product_id: product.id).quantity).to eq(5)
         end
 
         it 'increase quantity of cart item by given number if product is already in cart' do
+          request
           post "/cart/#{product.id}", params: { quantity: 5 }
           follow_redirect!
           expect(user.shopping_cart.cart_items.find_by(product_id: product.id).quantity).to eq(10)
@@ -56,6 +60,7 @@ RSpec.describe 'CartItem', type: :request do
         let(:params) { { quantity: 'a' } }
 
         it 'not allows to provide not numerical value' do
+          request
           expect(response.body).to include('Quantity is not a number')
         end
       end
@@ -64,10 +69,12 @@ RSpec.describe 'CartItem', type: :request do
         let(:params) { { quantity: nil } }
 
         it 'convert quantity nil value to quantity equals one' do
+          request
           expect(user.shopping_cart.cart_items.find_by(product_id: product.id).quantity).to eq(1)
         end
 
         it 'convert quantity nil value to quantity equals one when product already in cart' do
+          request
           expect do
             post "/cart/#{product.id}", params: params
             follow_redirect!
