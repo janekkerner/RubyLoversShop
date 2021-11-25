c69f558f60d51d3d5cd87f6cba54e36a882aec7e
Jan <janek.kerner@gmail.com>
2021-11-25 22:45:47 +0100

    test: refactor of shopping cart request spec

--- spec/requests/shopping_cart_spec.rb
+++ spec/requests/shopping_cart_spec.rb
@@ -2,93 +2,91 @@
 
 require 'rails_helper'
 
+# deeper nesting required for specified context
+# rubocop:disable RSpec/NestedGroups
+
 RSpec.describe 'ShoppingCarts', type: :request do
-  let!(:user) { create(:user, shopping_cart: create(:shopping_cart)) }
-  let!(:product) { create(:product) }
+  let(:user) { create(:user, shopping_cart: create(:shopping_cart)) }
 
   describe 'GET /cart' do
-    it 'has redirect http status when user not sign in' do
-      get '/cart'
-      expect(response).to have_http_status(:redirect)
+    subject(:request) { get '/cart' }
+
+    context 'when anonymous user visiting' do
+      it 'has redirect http status' do
+        request
+        expect(response).to have_http_status(:redirect)
+      end
+
+      it 'shows log in page' do
+        request
+        follow_redirect!
+        expect(response.body).to include('You need to sign in or sign up before continuing')
+      end
     end
 
-    it 'has success http status when user signed in' do
-      sign_in user
-      get '/cart'
-      expect(response).to have_http_status(:success)
-    end
+    context 'when user signed in' do
+      before do
+        sign_in user
+      end
 
-    it 'shows shopping cart if user is signed in' do
-      sign_in user
-      get '/cart'
-      expect(response.body).to include('Your shopping cart is empty')
+      it 'has success http status' do
+        request
+        expect(response).to have_http_status(:success)
+      end
+
+      it 'shows shopping cart' do
+        request
+        expect(response.body).to include('Your shopping cart is empty')
+      end
     end
   end
 
   describe 'POST /cart' do
+    subject(:request) { post '/cart', params: params }
+
+    let!(:product) { create(:product) }
+    let(:params) { { params: {} } }
+
     context 'when user is not signed in' do
-      it 'redirect to sign in page if user not signed in' do
-        post "/cart/#{product.id}"
+      it 'redirect to sign in page' do
+        request
         follow_redirect!
         expect(response.body).to include('You need to sign in or sign up before continuing.')
       end
     end
 
     context 'when user signed in' do
-      let!(:product2) { create(:product) }
+      subject(:request) { post '/cart', params: params }
+
       let(:cart_item) { create(:cart_item, shopping_cart: user.shopping_cart, product: product) }
-      let(:cart_item2) { create(:cart_item, shopping_cart: user.shopping_cart, product: product2) }
-      let(:cart_item3) { create(:cart_item, shopping_cart: user.shopping_cart, product: create(:product)) }
-      let(:cart_items_params) do
-        {
-          cart_items: {
-            cart_item.id => {
-              quantity: '5'
-            },
-            cart_item2.id => {
-              quantity: '10'
-            }
-          }
-        }
-      end
+      let(:cart_item2) { create(:cart_item, shopping_cart: user.shopping_cart, product: create(:product)) }
+      let(:params) { {} }
 
       before do
         sign_in user
       end
 
-      it 'has success http status' do
-        post "/cart/#{product.id}"
-        follow_redirect!
-        expect(response).to have_http_status(:success)
-      end
-
-      it 'renders success notification after adding product to cart' do
-        post "/cart/#{product.id}"
-        follow_redirect!
-        expect(response.body).to include("Product #{product.name} has been added to your shopping cart")
-      end
-
-      it 'can add product to shopping cart' do
-        expect do
-          post "/cart/#{product.id}"
-          follow_redirect!
+      context 'with quantity provieded as hash for each cart item' do
+        let(:params) do
+          { cart_items: { cart_item.id => { quantity: '5' }, cart_item2.id => { quantity: '10' } } }
         end
-          .to change(user.shopping_cart.cart_items, :count)
+
+        it 'can change quantity of many products in shopping cart' do
+          request
+          expect(cart_item.reload.quantity).to eq(params[:cart_items][cart_item.id][:quantity].to_i)
+          expect(cart_item2.reload.quantity).to eq(params[:cart_items][cart_item2.id][:quantity].to_i)
+        end
       end
 
-      it 'can change quantity of many products in shopping cart' do
-        post '/cart', params: cart_items_params
-        follow_redirect!
-        expect(cart_item.reload.quantity).to eq(cart_items_params[:cart_items][cart_item.id][:quantity].to_i)
-        expect(cart_item2.reload.quantity).to eq(cart_items_params[:cart_items][cart_item2.id][:quantity].to_i)
-      end
+      context 'with quantity provided as zero value' do
+        let(:params) { { cart_items: { cart_item.id => { quantity: '0' } } } }
 
-      it 'removes product from shopping cart if quantity was set to zero' do
-        expect(CartItem.where(id: cart_item3.id)).to exist
-        post '/cart', params: { cart_items: { cart_item3.id => { quantity: '0' } } }
-        follow_redirect!
-        expect(CartItem.where(id: cart_item3.id)).not_to exist
+        it 'removes product from shopping cart if quantity was set to zero' do
+          request
+          expect(CartItem.where(id: cart_item.id)).not_to exist
+        end
       end
     end
   end
 end
+# rubocop:enable RSpec/NestedGroups
